077ae008c9e21b7a15ebbbdbc70c6587e2f4f966
angry-cellophane <email.com>
2019-12-27 23:44:02 +0000

    Add a simple test to verify jenkins is running

--- jenkins-master-image/src/test/groovy/org/ka/jenkins/masterless/image/junkins/JunkinsTest.groovy
+++ jenkins-master-image/src/test/groovy/org/ka/jenkins/masterless/image/junkins/JunkinsTest.groovy
@@ -1,6 +1,36 @@
 package org.ka.jenkins.masterless.image.junkins
 
+import org.apache.commons.io.FileUtils
+import spock.lang.Shared
 import spock.lang.Specification
 
+import java.nio.file.Files
+
 class JunkinsTest extends Specification {
+
+    @Shared Junkins junkins
+
+    void setupSpec() {
+        def root = Files.createTempDirectory('jenkins').toFile()
+        root.deleteOnExit()
+
+        def plugins = root.toPath().resolve('plugins').toFile()
+        plugins.mkdir()
+
+        FileUtils.copyDirectory(new File("./build/plugins"), plugins);
+
+        junkins = Junkins.builder()
+                .rootDir(root)
+                .warExplodedDir(new File("./build/war"))
+                .build()
+    }
+
+    void cleanupSpec() {
+        junkins.stop()
+    }
+
+    void 'junkins is running'() {
+        expect:
+        junkins.running
+    }
 }
