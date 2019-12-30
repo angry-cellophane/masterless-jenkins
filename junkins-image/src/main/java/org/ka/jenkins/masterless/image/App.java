5b538c03d53fc636d47ba48c860b77a6c31e7d23
angry-cellophane <email.com>
2019-12-30 00:04:30 +0000

    Use default jenkins config or override from /opt/jenkins_data

--- junkins-image/src/main/java/org/ka/jenkins/masterless/image/App.java
+++ junkins-image/src/main/java/org/ka/jenkins/masterless/image/App.java
@@ -1,10 +1,12 @@
 package org.ka.jenkins.masterless.image;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.ka.jenkins.masterless.image.junkins.Junkins;
 import org.ka.jenkins.masterless.image.junkins.JunkinsException;
 
 import java.io.File;
+import java.io.IOException;
 
 public class App {
 
@@ -16,25 +18,35 @@ public class App {
 
     static final String PIPELINE = "node('master') { sh 'echo hello' }";
 
-    public static void main(String[] args) {
-        var rootDir = envVar(EnvVars.ROOT_DIR);
-        var warExploded = envVar(EnvVars.WAR_EXPLODED);
+    public static void main(String[] args) throws IOException {
+        var rootDir = fileFromEnvVar(EnvVars.ROOT_DIR);
+        var warExploded = fileFromEnvVar(EnvVars.WAR_EXPLODED);
+
+        var optData = new File("/opt/jenkins_data");
+        if (optData.exists()) {
+            FileUtils.copyDirectoryToDirectory(optData, rootDir);
+        }
 
         var junkins = Junkins.builder()
-                .rootDir(new File(rootDir))
-                .warExplodedDir(new File(warExploded))
+                .rootDir(rootDir)
+                .warExplodedDir(warExploded)
                 .build();
         junkins.runOneOffPipeline(PIPELINE);
         junkins.stop();
         System.exit(0);
     }
 
-    static String envVar(String name) {
+    static File fileFromEnvVar(String name) {
         String value = System.getenv(name);
         if (StringUtils.isBlank(value)) {
             throw new JunkinsException("required env var " + name + " not defined");
         }
 
-        return value;
+        var file = new File(value);
+        if (!file.exists()) {
+            throw new JunkinsException("file " + value + " from env var " + name + " does not exist");
+        }
+
+        return file;
     }
 }
